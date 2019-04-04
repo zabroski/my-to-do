e2746b0665b1c9a3b5896834606abc8390d30d9f
Issouf zabre <issoufzabre@Issoufs-MacBook-Pro.local>
2019-04-03 23:36:01 -0400

    set days names

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  getCurrentDay(){
+    let days= {
+      1: "Monday",
+      2: "Tuesday",
+      3: "Wenesday",
+      4: "Thursday",
+      5: "Friday",
+      6: "Saturday",
+      7: "Sunday"  
+    };
+
+    let date = new Date();
+    let number = date.getDay();
+
+    return days[number]; 
+  }
+
   render() {
     return (
       <div className="App">
         <div className="date">January 27, 2019</div>
-        <div className="day">Wenesday</div>
+        <div className="day">{this.getCurrentDay()}</div>
       </div>
     );
   }
