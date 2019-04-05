f88adfb36a3a2d25d5476f6186931944b1626afb
Issouf zabre <issoufzabre@Issoufs-MacBook-Pro.local>
2019-04-05 11:33:15 -0400

    fix date, months, and year

--- src/App.js
+++ src/App.js
@@ -21,12 +21,29 @@ class App extends Component {
     return days[number]; 
   }
 
-getCurrentDate(){
-  let date = "November 18, 2019";
+  getCurrentDate(){
+    let date= new Date();
+    let number = date.getMonth() + 1;
+    let day = date.getDate();
+    let year = date.getFullYear();
 
-  return date;
-}
+    let months= {
+      1: "January",
+      2: "February",
+      3: "March",
+      4: "April",
+      5: "May",
+      6: "June",
+      7: "July",
+      8: "August",
+      9: "September",
+      10: "October",
+      11: "November",
+      12: "December"
+    };
 
+    return day + " " + months[number] + " " + year;
+  }
 
   render() {
     return (
